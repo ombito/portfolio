935614334a178a638ab844ff1a7955e5e417e288
unknown <alvinombito09@gmail.com>
2023-02-13 16:47:36 +0300

    styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = () => {
   return (
     <div className={color ? "header header-bg": "header"}>
       <Link to="/">
-        <h1>Portfolio</h1>
+        <h1>Ombito</h1>
       </Link>
       <ul className="nav-menu">
         <li>
