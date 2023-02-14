26a240fdc862764bb15a67371439bb3f902a2b18
unknown <alvinombito09@gmail.com>
2023-02-14 23:28:40 +0300

    styled about component

--- src/components/HeroImg3.jsx
+++ src/components/HeroImg3.jsx
@@ -6,12 +6,12 @@ const HeroImg3 = () => {
   return (
     <section>
       <div className="about-me">
-        <h1>About Me</h1>
-        <p>I am an optimist, a data nerd, technologist, open-source enthusiast and a family man. I am a proud Kenyan who gets excited about travelling, swimming and book reading. When I am not at my desk, you can find me on my farm or spending weekends with my family and friends. I have a holistic approach to strategic thinking and hands on management.</p>
+        <h2>About Me</h2>
+        <p>I am an optimist, a data nerd, technologist, open-source enthusiast and a family man. I am a proud <br/> Kenyan who gets excited about travelling, swimming and book reading. When I am not at my desk, <br/> you can find me on my farm or spending weekends with my family and friends. I have a holistic approach to strategic <br/> thinking and hands on management.</p>
         <div className="wrapper">
-          <div className="content">
+          <div className="journey">
             <h2>My Journey</h2>
-            <p1>Techlink Systems <br/> Systems Engineer, Present <br/> Nairobi, Kenya</p1>
+            <p>Techlink Systems <br/> Systems Engineer, Present <br/> Nairobi, Kenya</p>
           </div>
         </div>
       </div>
