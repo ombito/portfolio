1ba67d9c67ad1db1027adb4bdf6c7bdbc430851c
unknown <alvinombito09@gmail.com>
2023-02-15 22:10:06 +0300

    imported all files to about page

--- src/components/HeroImg3.jsx
+++ src/components/HeroImg3.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './HeroImg3Styles.css';
-import giraffe from "../images/giraffe.jpg";
+import alvin from "../images/alvin.jpg";
 
 const HeroImg3 = () => {
   return (
-    <section>
+    <section className="hero-section">
+      <div>
+      <img className='profile' src={alvin} width="500" height="540" />
+      </div>
       <div className="about-me">
         <h2>About Me</h2>
-        <p>I am an optimist, a data nerd, technologist, open-source enthusiast and a family man. I am a proud <br/> Kenyan who gets excited about travelling, swimming and book reading. When I am not at my desk, <br/> you can find me on my farm or spending weekends with my family and friends. I have a holistic approach to strategic <br/> thinking and hands on management.</p>
+        <p>I am an optimist, a data nerd, technologist, open-source enthusiast and a family man. I am a proud <br/> Kenyan who gets excited about travelling, swimming and book reading. When I am not at my desk, <br/> you can find me on my farm or spending weekends with my family and friends. I have a <br/>holistic approach to strategic thinking and hands on management.</p>
         <div className="wrapper">
           <div className="journey">
-            <h2>My Journey</h2>
-            <p>Techlink Systems <br/> Systems Engineer, Present <br/> Nairobi, Kenya</p>
+            <h3>My Journey</h3>
+            <p1>Techlink Systems <br/> Systems Engineer, Present <br/> Nairobi, Kenya</p1>
           </div>
         </div>
       </div>
